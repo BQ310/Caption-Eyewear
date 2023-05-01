7a565bf6ecb8c63fd2d43058c969fa6bb08ea547
fcharles <fcharles@ucsc.edu>
2023-05-01 00:06:45 -0700

    Added the text alignment characteristic.

--- ArduinoBLEPeriphal/ArduinoPeripheral.ino
+++ ArduinoBLEPeriphal/ArduinoPeripheral.ino
@@ -2,14 +2,17 @@
 #include <ArduinoBLE.h>
 // For Bluetooth
   const char* ServiceUUID = "fd4733c0-def3-11ed-b5ea-0242ac120002";
-  const char* examplCharUUID = "fd4733c1-def3-11ed-b5ea-0242ac120002";
+  const char* SpeechTextCharUUID = "fd4733c1-def3-11ed-b5ea-0242ac120002";
+  const char* TextAlignmentCharUUID = "fd4733c2-def3-11ed-b5ea-0242ac120002";
   BLEService ExBLE(ServiceUUID);
-  BLECharacteristic speechText(examplCharUUID, BLEWrite, 50, false);
+  BLECharacteristic speechText(SpeechTextCharUUID, BLEWrite, 50, false);
+  BLEBoolCharacteristic textAlignment(TextAlignmentCharUUID, BLEWrite);  // false:  Upper 3 rows,   True:  Lower 3 rows
 
 void setup() {
   // put your setup code here, to run once:
+
   Serial.begin(115200);
-  while(!Serial);
+  while(!Serial); // remove this if you want to run without serial
 
   if (!BLE.begin()) {
     Serial.println("Error: BLUETOOTH NOT STARTING");
@@ -19,6 +22,7 @@ void setup() {
   BLE.setLocalName("Text Test For App");
   BLE.setAdvertisedService(ExBLE);
   ExBLE.addCharacteristic(speechText);
+  ExBLE.addCharacteristic(textAlignment);
   BLE.addService(ExBLE);
   BLE.advertise();
 }
@@ -27,10 +31,22 @@ void loop() {
   // put your main code here, to run repeatedly:
   BLEDevice central = BLE.central();
   delay(500);
-  bool prevResult = false;
   if(central) {
     Serial.println("Connected to Central");
     while(central.connected()){
+
+      /*Text Alignment for screen*/
+      if (textAlignment.written()) {
+        if (textAlignment == true) {
+          // Do Something here
+          Serial.println("Text Alignment lower three rows.");
+        } else {
+          // Do Something here
+          Serial.println("Text Alignment upper three rows.");
+        }
+      }
+
+      /*Speech to text from phone */ 
       if (speechText.written()) {
         for (int i = 0; i < speechText.valueLength(); ++i) {
           Serial.print(char(speechText.value()[i]));
